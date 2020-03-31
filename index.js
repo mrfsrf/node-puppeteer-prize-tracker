3f70bd12eb249e29ae13fca4b1d8b2c3f22ffd28
Luka Juras <mrfsrf@gmail.com>
2020-03-31 09:35:50 +0200

    refactor the code to work with multiple urls

--- index.js
+++ index.js
@@ -10,54 +10,57 @@ const PASS = process.env.MAIL_PASS;
 
 let trackingUrl = '';
 
-/* check if env variables loaded */
-console.log(`Email address is ${EMAIL} and password is ${PASS}`);
 
 const PRODUCT_URLS = {
-  productOne: 'cowin-SE7-Cancelling-Headphones-Comfortable-Black/dp/B07DB4WNFW', // Product 1 - Playstation
-  productTwo: 'Google-PIXEL-G013C-64GB-Smartphone/dp/B07MTDQP7F', // Google Pixel 3XL
-  productThree: 'Nintendo-Switch-Neon-eShop-Voucher/dp/B07TB3VS2C' // Nitendo switch
+  // productOne: 'dp/B07Q1YXVWH/ref=psdc_4085761_t1_B07DB4WNFW', // Product 1 - headphones
+  productTwo: 'dp/B07MTDQP7F', // Google Pixel 3XL
+  // productThree: 'Nintendo-Switch-Neon-eShop-Voucher/dp/B07TB3VS2C' // Nitendo switch
+  productFour: 'dp/B07Q3RX9MY'
+  // https://www.amazon.co.uk/Dewdrop-Display-Smartphone-Ultra-wide-Sim-Free-Black/dp/B07Q3RX9MY?ref_=s9_apbd_otopr_hd_bw_b5qselD&pf_rd_r=66CXGMDETK4WKVPG5B69&pf_rd_p=6009d7e0-7bae-5e6b-9e98-f5228ba53cfd&pf_rd_s=merchandised-search-11&pf_rd_t=BROWSE&pf_rd_i=5362060031
 };
 
 const makeUrl = (product) => {
   return `https://www.amazon.co.uk/${product}`;
 }
 
+
 async function configureBrowser() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  for (const [Product, ProductUrl] of Object.entries(PRODUCT_URLS)) {
-    trackingUrl = makeUrl(ProductUrl);
-    await page.goto(trackingUrl);
-  }
-  return page;
+    for (const [Product, ProductUrl] of Object.entries(PRODUCT_URLS)) {
+      trackingUrl = makeUrl(ProductUrl);
+      console.log(`Product ${Product} and url is ${trackingUrl}`)
+      startTracking(trackingUrl);
+    }
 }
 
 
 async function checkPrice(page) {
-  await page.reload();
-  let html = await page.evaluate(() => document.body.innerHTML);
-  // console.log(html);
+    await page.reload();
+    let html = await page.evaluate(() => document.body.innerHTML);
+    // actualy it is #priceblock_saleprice !!!
+    $('#priceblock_ourprice', html).each(function () {
+      let dollarPrice = $(this).text();
+      const initialPrice = Number(dollarPrice.replace(/[^0-9.-]+/g, "")); // set first time only!
 
-  $('#priceblock_dealprice', html).each(function () {
-    let dollarPrice = $(this).text();
-    // console.log(dollarPrice);
-    let currentPrice = Number(dollarPrice.replace(/[^0-9.-]+/g, ""));
+      console.log(dollarPrice);
+      let currentPrice = Number(dollarPrice.replace(/[^0-9.-]+/g, ""));
 
-    if (currentPrice < 300) {
-      console.log("BUY!!!! " + currentPrice);
-      sendNotification(currentPrice);
-    }
-  });
+      if (currentPrice < initialPrice) {
+        console.log("BUY!!!! " + currentPrice);
+        sendNotification(currentPrice);
+      }
+    });
 }
 
-async function startTracking() {
-  const page = await configureBrowser();
+async function startTracking(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    // const page = await configureBrowser();
+    await page.goto(url);
+    let job = new CronJob('* */30 * * * *', function () { //runs every 30 minutes in this config
+      checkPrice(page);
+    }, null, true, null, null, true);
+    job.start();
 
-  let job = new CronJob('* */30 * * * *', function () { //runs every 30 minutes in this config
-    checkPrice(page);
-  }, null, true, null, null, true);
-  job.start();
 }
 
 
@@ -72,7 +75,7 @@ async function sendNotification(price) {
   });
 
   let textToSend = 'Price dropped to ' + price;
-  let htmlText = `<a href=\"${url}\">Link</a>`;
+  let htmlText = `<a href=\"${trackingUrl}\">Link</a>`;
 
   let info = await transporter.sendMail({
     from: '"Price Tracker" <*****@gmail.com>',
@@ -85,4 +88,4 @@ async function sendNotification(price) {
   console.log("Message sent: %s", info.messageId);
 }
 
-startTracking();
+configureBrowser();
